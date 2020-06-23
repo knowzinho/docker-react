77042c09c361a4a5a324b1039a9bd6b6071115f1
Reginaldo De Araujo <rdear001@gold.ac.uk>
2020-06-23 19:42:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          compose worked...fixed it...how about now
         </p>
         <a
           className="App-link"
